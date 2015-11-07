809a3ba2b0009c309d267531a02ab706afd7bed4
James Hageman <jamesd@hageman.ca>
2015-11-07 14:13:17 -0500

    [George] Put accelerometer code in input.c

--- Copter/input.h
+++ Copter/input.h
@@ -2,6 +2,15 @@
 #define INPUT_H
 
 #include "gamestate.h"
+#include <FillPat.h>
+#include <I2CEEPROM.h>
+#include <LaunchPad.h>
+#include <OrbitBoosterPackDefs.h>
+#include <OrbitOled.h>
+#include <OrbitOledChar.h>
+#include <OrbitOledGrph.h>
+
+#include  "delay.h"
 
 void readSensorData(GameState* state);
 
